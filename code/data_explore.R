dad101701798d9fd97c14db08d12edce487ddb12
Clayton Glasgow <cglasgow@nd.edu>
2021-09-20 09:04:33 -0400

    updated code to match Clayton's fork

--- code/data_explore.R
+++ code/data_explore.R
@@ -20,3 +20,78 @@ all_mass %>%
 # Making a change to the code!!!
 dim(all_mass)
 
+# convert Chamber to character for graphical purposes
+all_mass_long$Chamber <- as.character(all_mass_long$Chamber)
+
+# Make plot looking at biomass change over time (by species)
+# not sure if this is the best way to visualize
+all_mass_long %>% filter(biomass_m2 > -1) %>%  # filter out missing data
+  # y = biomass, x= year, color by species
+  ggplot(aes(x = Year, y = biomass_m2, color = Treatment)) +
+  # plot as line graph, add trendline
+  geom_point() + geom_smooth(method = "lm", se = FALSE) +
+  # facet by species
+  facet_wrap(vars(species))
+
+# find average biomass per species/treatment/community/year
+all_mass_long %>% 
+  # group by year, community, treatment, species
+  group_by(Year, Community, Treatment, species) %>% 
+  # calculate mean
+  summarize(mean_bm = mean(biomass_m2)) -> all_mass_avg
+
+# plot MX community over time
+# filter by community, also filter out missing values
+all_mass_avg %>% filter(Community == "MX") %>% filter(mean_bm > -1) %>% 
+  # plot biomass over time, color by species
+  ggplot(aes(x = Year, y = mean_bm, color = species)) +
+  # plot as scatter plot, facet by treatment
+  geom_point() + facet_wrap(vars(Treatment)) + 
+  # add trend lines
+  geom_smooth(method = "lm", se = FALSE)
+
+# same plot as above, but with all communities
+all_mass_avg %>% filter(mean_bm > -1) %>%  # filter out missing values
+  # plot biomass over time, color by species
+  ggplot(aes(x = Year, y = mean_bm, color = species)) +
+  # plot as scatter plot, facet by treatment and community
+  geom_point() + facet_wrap(vars(Treatment, Community)) + 
+  # add trend lines
+  geom_smooth(method = "lm", se = FALSE)
+
+# now look at densities
+all_mass %>% 
+  # select densities
+  select(Year:Chamber, SCdensity_m2, SPdensity_m2, DIdensity_m2, OTHERdensity_m2) %>% 
+  # Reformat data from wide to long so that species is in one column
+  gather(key = species, value = density_m2, SCdensity_m2:OTHERdensity_m2) -> all_density_long
+
+# plot densities over time 
+all_density_long %>%   
+  # y = density, x= year, color by species
+  ggplot(aes(x = Year, y = density_m2, color = Treatment)) +
+  # plot as line graph, add trendline
+  geom_point() + geom_smooth(method = "lm", se = FALSE) +
+  # facet by species
+  facet_wrap(vars(species))
+
+# find average density per species/treatment/community/year
+all_density_long %>% 
+  # group by year, community, treatment, species
+  group_by(Year, Community, Treatment, species) %>% 
+  # calculate mean
+  summarize(mean_density = mean(density_m2)) -> all_density_avg
+
+# plot mean density over time per community and treatment
+all_density_avg %>%  
+  # plot density over time, color by species
+  ggplot(aes(x = Year, y = mean_density, color = species)) +
+  # plot as scatter plot, facet by treatment and community
+  geom_point() + facet_wrap(vars(Treatment, Community)) + 
+  # add trend lines
+  geom_smooth(method = "lm", se = FALSE)
+
+
+
+
+
